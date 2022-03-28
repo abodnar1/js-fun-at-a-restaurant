4920052fa42b365115023d2b664d68bcb2fddd75
Amber Bodnar <amber.bodnar@gmail.com>
2022-03-27 22:32:02 -0400

    Add Restaurant and Restaurant Test

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,30 @@
+function createRestaurant(restName) {
+  return restObject = {
+    name: restName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  };
+};
+
+
+
+function addMenuItem(restObject, menuItem) {
+  return
+};
+
+
+
+
+
+
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant,
+  addMenuItem,
   // removeMenuItem
 }
